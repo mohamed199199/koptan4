81f332a05ca6db95ecc3bf1d74d408c6e0b12ec1
mohamed199199 <mohamedkhalil80600@gmail.com>
2025-05-05 18:45:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import streamlit as st
 
 st.write("تم فتح التطبيق")
 
+# مسار الملف
+file_path = 'output/html_files/html_1.html'
+
 try:
-    with open('output/html_files/html_1.html', 'r', encoding='utf-8') as file:
+    # محاولة فتح الملف وقراءة محتواه
+    with open(file_path, 'r', encoding='utf-8') as file:
         html_code = file.read()
         st.markdown(html_code, unsafe_allow_html=True)
 except FileNotFoundError:
-    st.error("الملف مش موجود: output/html_files/html_1.html")
+    # إذا كان الملف غير موجود
+    st.error(f"الملف مش موجود: {file_path}")
 except Exception as e:
+    # في حالة وجود أي خطأ آخر
     st.error(f"حصل خطأ: {e}")
